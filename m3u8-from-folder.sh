e3b846a86b34acfd307e3fc8f4c3008178e23b7c
Dudu Akiva <duduke@gmail.com>
2017-02-13 22:19:50 +0200

    Update m3u8-from-folder.sh

--- m3u8-from-folder.sh
+++ m3u8-from-folder.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo "#EXTINF:0,Kids Movies,tvg-logo="http://mediasrv.lan/KidsCartoonMovies_logo.jpg"" > /var/www/html/kids-movies.m3u8
-tree /media/tower-2000-2/Kids-Movies -H http://mediasrv.lan/webdav/tower-2000-2/Kids-Movies -t -r | grep 'mp4\|mkv\|avi\|flv' | cut -d'"' -f2 >> /var/www/html/kids-movies.m3u8
+tree /media/tower-2000-2/Kids-Movies -H http://mediasrv.lan/webdav/tower-2000-2/Kids-Movies -t -r | grep 'mp4\|mkv\|avi\|flv' | cut -d'"' -f2 | shuf >> /var/www/html/kids-movies.m3u8
+
+echo "#EXTINF:0,Kids Movies,tvg-logo="http://mediasrv.lan/KidsTV_logo.jpg"" > /var/www/html/kids-tv.m3u8
+tree /media/tower-2000-2/Kids-TV -H http://mediasrv.lan/webdav/tower-2000-2/Kids-TV -t -r | grep 'mp4\|mkv\|avi\|flv' | cut -d'"' -f2 | shuf >> /var/www/html/kids-tv.m3u8
+
