b8dc8a3d1e7da80d6121e9270e9e6f0dd61266d6
Dudu Akiva <duduke@gmail.com>
2017-03-14 21:36:53 +0200

    Update creat-vlm-playlist.sh

--- creat-vlm-playlist.sh
+++ creat-vlm-playlist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "# VLC media player VLM command batch" > /root/kids-movies.vlm
-echo "# http://www.videolan.org/vlc/" >> /root/kids-movies.vlm
-# echo "new Kids-Movies vod enabled" >> /root/kids-movies.vlm
-find /media/tower-2000-2/Kids-TV/ -type f | sed 's/^/setup Kids-Movies input "/' | sed 's/.*/&"/' >> /root/kids-movies.vlm
-# echo "setup Kids-Movies option file-caching=300" >> /root/kids-movies.vlm
+echo "# VLC media player VLM command batch" > ~/playlist.conf
+echo "# http://www.videolan.org/vlc/" >> ~/playlist.conf
+find /media/tower-2000-2/Kids-TV/ -type f | sed 's/^/setup channel3 input "/' | sed 's/.*/&"/' | shuf >> ~/playlist.conf
+
+ nohup cvlc -I telnet --telnet-password password  --file-logging --log-verbose=1 --logfile="vlc.log" --vlm-conf vlm.conf & echo $! > vlc.pid
